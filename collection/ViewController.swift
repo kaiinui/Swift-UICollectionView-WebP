c68017d9438f46429bcd3bce8dd0309fda0e5740
Kai INUI <lied.der.optik@gmail.com>
2014-06-17 22:26:49 +0900

    Initial Commit

---
+++ collection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  collection
+//
+//  Created by kaiinui on 2014/06/17.
+//  Copyright (c) 2014年 PeitLoan Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
